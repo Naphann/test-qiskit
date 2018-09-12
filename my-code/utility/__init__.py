9884c6db37e249c87b7992ae727b055bc08c76c0
Naphan Benchasattabuse <naphanben@gmail.com>
2018-09-12 17:02:03 +0700

    refactor utility function into utility package

---
+++ my-code/utility/__init__.py
@@ -0,0 +1,2 @@
+from util import Util
+from grover import Grover
---
+++ my-code/utility/grover.py
@@ -0,0 +1,27 @@
+from util import Util
+
+class Grover:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def inv_around_mean(circuit, qs, anc):
+        n = len(qs)
+        for i in range(n):
+            circuit.h(qs[i])
+        for i in range(n):
+            circuit.x(qs[i])
+        circuit.h(qs[0])
+        Util.n_control_not(circuit, [qs[i] for i in range(1, n)], qs[0], anc)
+        circuit.h(qs[0])
+        for i in range(n):
+            circuit.x(qs[i])
+        for i in range(n):
+            circuit.h(qs[i])
+
+    @staticmethod
+    def init_grover(circuit, f_input, f_output):
+        for j in range(len(f_input)):
+            circuit.h(f_input[j])
+        circuit.x(f_output)
+        circuit.h(f_output)
